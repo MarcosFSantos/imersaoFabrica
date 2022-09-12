a03d677d2a150aeff4bd5e75e54405a9c5d486a9
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-09-12 19:56:48 -0300

    main and comments

--- functions.py
+++ functions.py
@@ -2,8 +2,15 @@ import math
 from typing import List
 import math
 
+#função para calcular os valores de x' e x" com a formula de bhaskara
 def bhaskara(a, b, c, delta):
     list= []
+
+    #convertendo String para float
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    delta = float(delta)
 
     deltaSqrt = math.sqrt(delta)
 
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from functions import bhaskara
 
-x = bhaskara(4, 3, 2, 20)
+#Pedindo os valores ao usuário
+print("digite o valor de 'a': ")
+a = input()
+print("digite o valor de 'b': ")
+b = input()
+print("digite o valor de 'c': ")
+c = input()
+print("digite o valor de 'delta': ")
+delta = input()
+
+#chamando a função que vai calcular o valor de bhaskara
+x = bhaskara(a, b, c, delta)
+
+#imprimindo o valor na tela
 print(x)
