295026d1d94d89072d0a814637d5a53da759fe77
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-09-15 23:06:14 -0300

    Home page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def bomDia(request):
+    texto = "<html><body><h1>Bom dia!!!</h1></body></html>"
+    return HttpResponse(texto)
+
+def boaNoite(request):
+    texto = "<html><body><h1>Boa noite!!!</h1></body></html>"
+    return HttpResponse(texto)
+
+def home(request):
+    return render(request, template_name='home.html')
--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import bomDia, boaNoite, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bomDia/', bomDia),
+    path('boaNoite/', boaNoite),
+    path('home/', home),
 ]
