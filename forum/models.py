79b5e2de32fdebe9878e015e37211e7d1f8eeccd
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-09-16 20:23:19 -0300

    Criando a classe Topico no models.py

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Classe que representa os tópicos onde os usuários vão debater assuntos diversos no Forum de conversas.
+class Topico(models.Model):
+    Titulo = models.CharField(max_length=80, null=False, blank=False) #Título obrigatório do tópico.
+    Autor = models.CharField(max_length=20, null=False, blank=False) #Nome obrigatório do autor do tópico.
+    
+    
