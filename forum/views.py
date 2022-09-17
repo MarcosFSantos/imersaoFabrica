c8084e5faaf7c4246bc4670bc05df9978ebf8fc6
Marcos Vinícius <marcos.franca1673@gmail.com>
2022-09-16 21:03:43 -0300

    Atualizando a pagina principal para a visualizacao dos topicos

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from forum.models import Topico
 
+# função view da página principal responsável por contruir a página de acordo com o banco de dados. 
 def pagPrincipal(request):
-    return render(request, 'pagPrincipal.html')
+    topicos = Topico.objects.all()
+    data = {}
+    data['topicos'] = topicos
+    return render(request, 'pagPrincipal.html', data)
